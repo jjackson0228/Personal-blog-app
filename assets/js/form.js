0a7f0c0cedcff512f16250c6c9b9382cc41d9143
James Jackson <jamesjackson@Jamess-Air.lan>
2024-06-20 14:49:17 -0400

    added css for light and dark mode

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,14 +1,14 @@
 //added event listener and const for toggle button to switch from light and dark mode
 document.addEventListener("DOMContentLoaded", () => {
-  const toggleButton = document.getElementById("#mode-toggle");
-
+  const toggleButton = document.querySelector("#mode-toggle");
+  const body = document.body;
   //adding theme to be stored in local data
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme) {
     body.classList.add(savedTheme);
   } else {
     //default to light mode
-    body.classList.add(".light-mode");
+    body.classList.add("light-mode");
   }
   //adding local storage to even listener click
   toggleButton.addEventListener("click", () => {
@@ -16,6 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
       body.classList.remove("light-mode");
       body.classList.add("dark-mode");
       localStorage.setItem("theme", "dark-mode");
+    } else {
+      body.classList.remove("dark-mode");
+      body.classList.add("light-mode");
+      localStorage.setItem("theme", "light-mode");
     }
   });
 });
