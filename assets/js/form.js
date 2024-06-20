241940d644eea463f261dd2ab2ea246fee42e2e7
James Jackson <jamesjackson@Jamess-Air.lan>
2024-06-20 13:52:18 -0400

    adding toggle button event listener to form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,6 @@
 //added event listener and const for toggle button to switch from light and dark mode
 document.addEventListener("DOMContentLoaded", () => {
-  const toggleButton = document.getElementById("mode-toggle");
+  const toggleButton = document.getElementById("#mode-toggle");
 
   //adding theme to be stored in local data
   const savedTheme = localStorage.getItem("theme");
@@ -8,6 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     body.classList.add(savedTheme);
   } else {
     //default to light mode
-    body.classList.add("light-mode");
+    body.classList.add(".light-mode");
   }
+  //adding local storage to even listener click
+  toggleButton.addEventListener("click", () => {
+    if (body.classList.contains("light-mode")) {
+      body.classList.remove("light-mode");
+      body.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark-mode");
+    }
+  });
 });
