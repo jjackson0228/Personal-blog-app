c0b79552000c664548e8850ea881ff62883c5e3f
James Jackson <jamesjackson@Jamess-Air.lan>
2024-06-21 17:17:17 -0400

    added form.js and started to fix light dark mode toggle button to show image

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,10 +1,18 @@
 // started to add to the form .js
-const firstNameInput = document.querySelector("#first-name");
-const lastNameInput = document.querySelector("#last-name");
-const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#password");
-const signUpButton = document.querySelector("#sign-up");
+document
+  .getElementById("blog-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
 
-signUpButton.addEventListener("click", function (event) {
-  event.preventDefault();
-});
+    const formData = {
+      username: document.getElementById("username").value,
+      blogTitle: document.getElementById("blog-title").value,
+      blogContent: document.getElementById("blog-content").value,
+    };
+
+    // Save form data to local storage
+    localStorage.setItem("blogFormData", JSON.stringify(formData));
+
+    // Navigate to the blog.html page
+    window.location.href = "blog.html";
+  });
